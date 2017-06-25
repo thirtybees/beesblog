d7b2410d05b4781b2289a87faa458465dcef0400
dh42 <lesley@dh42.com>
2017-06-25 12:04:44 -0500

    add-ed home page hook

--- widgets/beesblogrecentposts/beesblogrecentposts.php
+++ widgets/beesblogrecentposts/beesblogrecentposts.php
@@ -41,6 +41,7 @@ class BeesBlogRecentPosts extends Module
         }
 
         $this->registerHook('displayLeftColumn');
+        $this->registerHook('displayHome');
 
         return true;
     }
@@ -83,5 +84,31 @@ class BeesBlogRecentPosts extends Module
     public function hookDisplayRightColumn()
     {
         return $this->hookDisplayLeftColumn();
+    }
+    /**
+     * Display in home page
+     *
+     * @return string
+     *
+     * @since 1.0.3
+     */
+    public function hookDisplayHome()
+    {
+        if (!Module::isEnabled('beesblog')) {
+            return '';
+        }
+
+        $recentPosts = BeesBlogPost::getPosts($this->context->language->id, 0, 5);
+        if (is_array($recentPosts)) {
+            foreach ($recentPosts as &$recentPost) {
+                $recentPost->link = BeesBlog::GetBeesBlogLink('beesblog_post', ['blog_rewrite' => $recentPost->link_rewrite]);
+            }
+        }
+        $this->context->smarty->assign([
+            'beesblogRecentPostsPosts' => $recentPosts,
+            'beesblogRecentPostsBlogUrl' => BeesBlog::getBeesBlogLink(),
+        ]);
+
+        return $this->display(__FILE__, 'views/templates/hooks/home.tpl');
     }
 }
