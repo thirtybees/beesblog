098d8660bc5325141901dcdfc0a965ed2bcb9833
Selwyn Orren <selwyn@linuxweb.co.za>
2017-11-03 04:20:33 +0200

    Update image dimension
    To better fit images on all posts and post items I have updated teh
    default post image width form 800 to 848 (Images are still responsive)

--- classes/BeesBlogImageType.php
+++ classes/BeesBlogImageType.php
@@ -31,11 +31,11 @@ class BeesBlogImageType extends \ObjectModel
     const LANG_TABLE = 'bees_blog_image_type_lang';
     const SHOP_TABLE = 'bees_blog_image_type_shop';
 
-    const POST_LIST_ITEM_WIDTH = 800;
+    const POST_LIST_ITEM_WIDTH = 848;
     const POST_LIST_ITEM_HEIGHT = 400;
-    const POST_DEFAULT_WIDTH = 800;
+    const POST_DEFAULT_WIDTH = 848;
     const POST_DEFAULT_HEIGHT = 400;
-    const CATEGORY_DEFAULT_WIDTH = 800;
+    const CATEGORY_DEFAULT_WIDTH = 848;
     const CATEGORY_DEFAULT_HEIGHT = 400;
 
     // @codingStandardsIgnoreStart
