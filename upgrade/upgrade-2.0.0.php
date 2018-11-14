e4261585ec4d670ac2d7cb39b9219d4865a14f7b
Gofenice Technologies <31014487+gofenice@users.noreply.github.com>
2018-11-14 19:19:01 +0530

    add meta fields to category and post (#27)
    * add meta fields to category and post
    
    * changes
    
    * Revert "changes"
    
    This reverts commit edc50a6fbf38acb2f0249d80e93e1283eff63b55.
    
    * Revert "add meta fields to category and post"
    
    This reverts commit d99b3e44aa1ad9f319bba963da6e41ea450e294b.
    
    * add meta fields to category and post
    
    * changes
    
    * changes
    
    * Sitemap - post image link, lastmod date

---
+++ upgrade/upgrade-2.0.0.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright (C) 2018 thirty bees
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@thirtybees.com so we can send you a copy immediately.
+ *
+ * @author    thirty bees <modules@thirtybees.com>
+ * @copyright 2018 thirty bees
+ * @license   Academic Free License (AFL 3.0)
+ */
+
+use BeesBlogModule\BeesBlogCategory;
+use BeesBlogModule\BeesBlogImageType;
+use BeesBlogModule\BeesBlogPost;
+
+if (!defined('_TB_VERSION_')) {
+    exit;
+}
+
+require_once __DIR__.'/../classes/autoload.php';
+
+function upgrade_module_2_0_0()
+{
+    $queries = [];
+    $queries[] = 'ALTER TABLE `'._DB_PREFIX_.bqSQL(BeesBlogPost::LANG_TABLE).'` ADD meta_title VARCHAR(128)';
+    $queries[] = 'ALTER TABLE `'._DB_PREFIX_.bqSQL(BeesBlogPost::LANG_TABLE).'` ADD meta_description VARCHAR(255)';
+    $queries[] = 'ALTER TABLE `'._DB_PREFIX_.bqSQL(BeesBlogPost::LANG_TABLE).'` ADD meta_keywords VARCHAR(255)';
+    $queries[] = 'ALTER TABLE `'._DB_PREFIX_.bqSQL(BeesBlogCategory::LANG_TABLE).'` ADD meta_title VARCHAR(128)';
+    $queries[] = 'ALTER TABLE `'._DB_PREFIX_.bqSQL(BeesBlogCategory::LANG_TABLE).'` ADD meta_description VARCHAR(255)';
+    $queries[] = 'ALTER TABLE `'._DB_PREFIX_.bqSQL(BeesBlogCategory::LANG_TABLE).'` ADD meta_keywords VARCHAR(255)';
+
+    foreach ($queries as $sql) {
+        try {
+            Db::getInstance()->execute($sql);
+        } catch (Exception $e) {
+        }
+    }
+
+    return true;
+}
