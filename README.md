3d4c26a66c39b2ceb4927fede35220105a551a49
Lesley Paone <lesley@dh42.com>
2019-06-26 19:06:16 -0500

    Validation change- i think I see the light

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Bees Blog
 
-## Bees Blog, thirty bees blog system
+bees blog is the official thirty bees blog module that comes standard with all thirty bees installations allowing you to make blog posts from your shop.
+
+## Description
 
 Bees blog is the blogging system that thirty bees uses to add blogging functionality to your thirty bees website.
+
+## License
+
+This software is published under the [Academic Free License 3.0](https://opensource.org/licenses/afl-3.0.php)
 
 ### Specific requirements for this repository
 
@@ -13,4 +19,11 @@ Contributors **must** follow the following rules:
 
 ## Roadmap
 
-[View](https://github.com/thirtybees/beesblog/blob/master/Roadmap.md)
+#### Short Term
+
+* None currently.
+
+#### Long Term
+
+* None currently.
+
--- beesblog.php
+++ beesblog.php
@@ -5,16 +5,16 @@
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
+ * that is bundled with this package in the file LICENSE.md.
  * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/afl-3.0.php
+ * https://opensource.org/licenses/afl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@thirtybees.com so we can send you a copy immediately.
  *
  * @author    thirty bees <modules@thirtybees.com>
- * @copyright 2017-2019 thirty bees
- * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ * @copyright 2019 thirty bees
+ * @license   Academic Free License (AFL 3.0)
  */
 
 use BeesBlogModule\BeesBlogCategory;
@@ -82,6 +82,9 @@ class BeesBlog extends Module
         $this->tab = 'front_office_features';
         $this->version = '1.0.4';
         $this->author = 'thirty bees';
+        $this->tb_min_version = '1.0.0';
+        $this->tb_versions_compliancy = '> 1.0.0';
+        $this->need_instance = 0;
 
         $this->controllers = ['category', 'post'];
         $this->bootstrap = true;
