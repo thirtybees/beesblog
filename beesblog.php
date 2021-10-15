49cede100c3445e229f24b159171e4fbf14fb6be
Petr Hučík <petr@getdatakick.com>
2021-10-15 09:27:04 +0200

    Version 1.4.0

--- beesblog.php
+++ beesblog.php
@@ -79,7 +79,7 @@ class BeesBlog extends Module
     {
         $this->name = 'beesblog';
         $this->tab = 'front_office_features';
-        $this->version = '1.3.0';
+        $this->version = '1.4.0';
         $this->author = 'thirty bees';
         $this->tb_min_version = '1.0.0';
         $this->tb_versions_compliancy = '> 1.0.0';
