a3aeed8fcf01c8e4112c168cf2ef7d67c8056daf
Petr Hučík <petr@getdatakick.com>
2023-10-05 22:56:07 +0200

    Fix XSS vulnerability
    Special thanks to Ulaş Deniz İlhan from Zigrin Security for discovering
    and reporting this vulnerability.

--- controllers/front/post.php
+++ controllers/front/post.php
@@ -73,7 +73,7 @@ class BeesBlogPostModuleFrontController extends ModuleFrontController
         }
         Media::addJsDef([
             'sharing_name' => addcslashes($post->title, "'"),
-            'sharing_url' => addcslashes(Tools::getHttpHost(true).$_SERVER['REQUEST_URI'], "'"),
+            'sharing_url' => addcslashes($post->link, "'"),
             'sharing_img' => addcslashes(Tools::getHttpHost(true).'/img/beesblog/posts/'.(int) $post->id.'.jpg', "'"),
         ]);
 
