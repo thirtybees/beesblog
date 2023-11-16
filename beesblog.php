21fe7621daec7dce328017a33d8c971994b686f9
Petr Hučík <petr@getdatakick.com>
2023-11-16 11:19:50 +0100

    Release beesblog v1.7.0

--- beesblog.php
+++ beesblog.php
@@ -87,7 +87,7 @@ class BeesBlog extends Module
     {
         $this->name = 'beesblog';
         $this->tab = 'front_office_features';
-        $this->version = '1.6.2';
+        $this->version = '1.7.0';
         $this->author = 'thirty bees';
         $this->tb_min_version = '1.0.0';
         $this->tb_versions_compliancy = '> 1.0.0';
